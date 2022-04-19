f06dfcd1a5478224f9d844204296d0692870b8c5
PAC Lab <rodgers.paclab@gmail.com>
2022-04-19 18:20:36 -0400

    Figured out the looping through a df, using a function and list comprehension. Please don't ask me what list comprehension means, stack overflow just used that word and I believe them.

--- Df_w_multis.py
+++ Df_w_multis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import loop
+
 ##NOTES: This is the current WIP.
 
 #I want to learn how the levels on a dataframe affect the different methods and properties and what you can and can't do with them.
@@ -14,107 +16,107 @@ print('Df_A','\n',Df_A)
 #This makes Df_B, a copy of Df_A, but with 'mouse','day', and 'trial' all set as indexes.
 Df_B=Df_A.set_index(['mouse','day','trial'])
 print('Df_B','\n',Df_B)
-#See how now, they're on different levels in a heirarchy, so that each mouse has a day, and each day has 3 trials.
+# See how now, they're on different levels in a heirarchy, so that each mouse has a day, and each day has 3 trials.
 
-#The df.info() command gives you information about the structure of your dataframe, and the type of data stored in it.
-#Look at the difference between the two. What are the columns? What's in the indexes?
+# The df.info() command gives you information about the structure of your dataframe, and the type of data stored in it.
+# Look at the difference between the two. What are the columns? What's in the indexes?
 print(Df_A.info())
 print(Df_B.info())
 
-#Keys() and columns are the same in a dataframe. Try it
+# Keys() and columns are the same in a dataframe. Try it
 print('Flat df keys()', Df_A.keys(),Df_A.columns)
 print('MultiIndex df keys()', Df_B.keys(),Df_B.columns)
-#Notice that it doesn't consider an index to be a column anymore, even if it looks like it's a column when the df is printed.
-#The ONLY columns in Df_B are 'rcp', 'speed', and 'variance.'
+# Notice that it doesn't consider an index to be a column anymore, even if it looks like it's a column when the df is printed.
+# The ONLY columns in Df_B are 'rcp', 'speed', and 'variance.'
 # Mouse, day, and trial are all indexes, and pandas interprets them as if they are on the rows.
 
-#Another way Pandas refers to columns and rows is as 'axes' like you'd have on a plot.
-#Axis 0 is the rows or the x axis, axis 1 is the columns or the y axis.
+# Another way Pandas refers to columns and rows is as 'axes' like you'd have on a plot.
+# Axis 0 is the rows or the x axis, axis 1 is the columns or the y axis.
 print('Flat df axis 0:  ', Df_A.axes[0])
 print('Flat df axis 1:  ', Df_A.axes[1])
 
-#Even in a MI dataframe, you still only have 2 axes.
-#It's just that instead of a series, the axis with multiple indexes will be a MultiIndex object, which looks kind of like an array.
+# Even in a MI dataframe, you still only have 2 axes.
+# It's just that instead of a series, the axis with multiple indexes will be a MultiIndex object, which looks kind of like an array.
 print('MultiIndex df axes:  ', Df_B.axes)
 
-#Each column in a dataframe is a series. This makes a Pandas series named 'mouse' from Df_A, with an autonumbered RangeIndex.
+# Each column in a dataframe is a series. This makes a Pandas series named 'mouse' from Df_A, with an autonumbered RangeIndex.
 mouse_series = Df_A['mouse']
 print('Mouse series','\n',mouse_series)
 
-#We can't use Df_B['mouse'] because in Df_B, 'mouse' is an index not a column.
-#Paste Df_B['mouse'] in the terminal and see what happens when you try.
-#Instead, we can reference the indexes by using the df.index object.
+# We can't use Df_B['mouse'] because in Df_B, 'mouse' is an index not a column.
+# Paste Df_B['mouse'] in the terminal and see what happens when you try.
+# Instead, we can reference the indexes by using the df.index object.
 print(Df_B.index)
 
-#Df.index.levels gives you more information about the indexes:
-#It gives you a list of the unique values in each index, and the order of the lists corresponds to the level of the index.
+# Df.index.levels gives you more information about the indexes:
+# It gives you a list of the unique values in each index, and the order of the lists corresponds to the level of the index.
 print(Df_B.index.levels)
-#So the mouse name is level 0, the day is level 2, the trial is level 3.
-#You can use df.index.names to get the names of an index ('mouse','day','trial') as a list
+# So the mouse name is level 0, the day is level 2, the trial is level 3.
+# You can use df.index.names to get the names of an index ('mouse','day','trial') as a list
 print(Df_B.index.names)
-#You can use df.swaplevel(a,b) to swap two levels in the heirarchy. Try it:
+# You can use df.swaplevel(a,b) to swap two levels in the heirarchy. Try it:
 Df_C=Df_B.swaplevel(0,1)
 
 ## Next up I want to talk about how to refer to data. .loc in particular is a pain if you don't know quite what it's doing.
-#Df.loc will try to locate data and parts of the df based on the index.
+# Df.loc will try to locate data and parts of the df based on the index.
 
-##SLICING BY ROWS:
-#In Df_A, whose only index is the autonumbering, .loc will only accept the row number as an argument and it will return the entire row as a series.
+## SLICING BY ROWS:
+# In Df_A, whose only index is the autonumbering, .loc will only accept the row number as an argument and it will return the entire row as a series.
 print(Df_A.loc[5])
 
-#For a MI dataframe, you can use any of the indexes, but they have to be in order.
+# For a MI dataframe, you can use any of the indexes, but they have to be in order.
 slice1 = Df_B.loc['F1_PAFT']
 slice2 = Df_B.loc['F1_PAFT','Mon',0]
-#What happens if you try them out of order? What if you skip a level? Try it in the console.
+# What happens if you try them out of order? What if you skip a level? Try it in the console.
 
-#Note that you can only select more than one value for the lowest level.
-#Df_B.loc['F1_PAFT','Mon',0:1] is valid, but Df_B.loc['F1_PAFT':'M2_PAFT','Mon',0] is not.
-#For that, use python's slice function. Options are slice(start value,end value, step).
-#To select everything, the syntax is slice(None)
+# Note that you can only select more than one value for the lowest level.
+# Df_B.loc['F1_PAFT','Mon',0:1] is valid, but Df_B.loc['F1_PAFT':'M2_PAFT','Mon',0] is not.
+# For that, use python's slice function. Options are slice(start value,end value, step).
+# To select everything, the syntax is slice(None)
 slice3 = Df_B.loc[(slice(None),'Mon',[0,1,2]),:]
 
-##SLICING BY COLUMNS:
-#Selecting an entire column is easy in either dataframe.
+## SLICING BY COLUMNS:
+# Selecting an entire column is easy in either dataframe.
 # For a df with no levels you can use df['column']. For a MI df, you can use that or df.loc[:,('column')
 Df_A_cols=Df_A[['mouse','day']]
 Df_B_cols1=Df_B[['rcp','speed']]
 Df_B_cols2=Df_B.loc[:,['rcp','speed']]
 
-#There are several ways of selecting specific values from a column. Nested criteria works the same on flat or hierarchical dfs:
+# There are several ways of selecting specific values from a column. Nested criteria works the same on flat or hierarchical dfs:
 Df_A_slow = Df_A[Df_A['speed']=='Low']
 Df_B_slow = Df_B[Df_B['speed']=='Low']
-#You can also use df.query(), where you write out your query expression as a string.
-#Use inplace=True if you want to change the df, getting rid of the records that don't match your query.
+# You can also use df.query(), where you write out your query expression as a string.
+# Use inplace=True if you want to change the df, getting rid of the records that don't match your query.
 Df_A.query('mouse== "F2_PAFT" and speed != "Low"')
 Df_B.query('mouse== "F2_PAFT" and speed != "Low"')
-#Look at the documentation for Pandas df.query() and python expressions for more info on this.
+# Look at the documentation for Pandas df.query() and python expressions for more info on this.
 
-#To take a subset of the data WITHOUT the headers/labels, you can use df.xs()
-#The syntax is df.xs(value(s)) and in a MI df, df.xs(value,level=#)
-#You can only use values that are in an index, so Df_A can only search by the autonumbered index.
+# To take a subset of the data WITHOUT the headers/labels, you can use df.xs()
+# The syntax is df.xs(value(s)) and in a MI df, df.xs(value,level=#)
+# You can only use values that are in an index, so Df_A can only search by the autonumbered index.
 DfA_xs = Df_A.xs(5)
 DfB_xs = Df_B.xs('Tues',level=1)
 # Be careful with xs and give any variables good descriptive names.
 # Since the headers are removed, a new df created with xs has no obvious way to know what your selection criteria was.
 
-##RESHAPING DATA
+## RESHAPING DATA
 
-#Ok now let's talk about reshaping and re-arranging data.
+# Ok now let's talk about reshaping and re-arranging data.
 # The easiest one is transposing the data: flipping what's on the columns vs the rows.
 # That's df.transpose() or just df.T
 DfA_T = Df_A.transpose()
 DfB_T = Df_B.T
-#This is useful since some methods only work on the rows.
+# This is useful since some methods only work on the rows.
 
-#Stacking and unstacking fields is another way to reshape the df.
+# Stacking and unstacking fields is another way to reshape the df.
 # Stacking is moving stuff from the columns headers to the rows
 # Unstacking is moving stuff from the rows to be column headers
-#Both of these really only make sense with a MI, otherwise you can do it but it'll just make a big long series.
+# Both of these really only make sense with a MI, otherwise you can do it but it'll just make a big long series.
 print(Df_A.stack())
-#See? Not very useful.
-#Try unstacking first, since we don't have any dfs with multi-indexes on the columns yet.
-#These lines will make a new df where the columns are also a multi-index, with mouse at the highest level.
-#The rows will be sorted by the trial number.
+# See? Not very useful.
+# Try unstacking first, since we don't have any dfs with multi-indexes on the columns yet.
+# These lines will make a new df where the columns are also a multi-index, with mouse at the highest level.
+# The rows will be sorted by the trial number.
 B_unstack_by_mouse = Df_B.unstack(['mouse'])
 B_unstack_by_mouse=B_unstack_by_mouse.swaplevel(0,1,axis=1)
 B_unstack_by_mouse = B_unstack_by_mouse.sort_index(axis=0,level=1)
@@ -132,11 +134,13 @@ Stacked_by_mouse = B_unstack_by_mouse.stack(level=1)
 Df_A['added']='New column'
 Df_B['added']='New column'
 
-#You can do basic operations with columns.
+# You can do basic operations with columns.
 Df_B['added'] = Df_B['rcp']-2
 
 # TODO: Now comes the stuff I really don't understand yet...
 #  a section on apply(), then on iterrows()/itercolumns() and looping if I can figure that out
 # Also include .drop and .dropna()
 
+##TODO: Ok just got this working, brain fried, will write up sensible explanation/guide on it later.
+Df_B['added']=[loop.findcondition(x, y) for x, y in zip(Df_B['speed'], Df_B['variance'])]
 # After I've finished I want to put it in a Jupyter notebook and add some practice exercises like Chris has in the example plots.
--- loop.py
+++ loop.py
@@ -1,16 +1,60 @@
+import pandas as pd
+
 ##NOTES: Want to use this to mess around with looping through the rows of a dataframe later.
+
+Df_A = pd.read_csv('Flat.csv')
+Df_B=Df_A.set_index(['mouse','day','trial'])
 
 speed= [0.25,0.1,0.25]
 varience= 0.1
-for sp in speed:
-    if sp == 0.25:
-        # if varience == 0.0001:
-        #     condition = 1
-        # elif varience == 0.01:
-        #     condition = 2
-        # elif varience == 0.1:
-        #     condition = 3
-        # else:
-        #     condition = 'unknown varience'
+# for sp in speed:
+#     if sp == 0.25:
+#         # if varience == 0.0001:
+#         #     condition = 1
+#         # elif varience == 0.01:
+#         #     condition = 2
+#         # elif varience == 0.1:
+#         #     condition = 3
+#         # else:
+#         #     condition = 'unknown varience'
+#     else:
+#         condition = 'nah'
+
+
+def findcondition(speed, varience):
+    if speed == 'Low':
+        if varience == 'Low':
+            condition = 1
+        elif varience == 'Medium':
+            condition = 2
+        elif varience == 'High':
+            condition = 3
+        else:
+            condition = 'unknown varience'
     else:
         condition = 'nah'
+    print(condition)
+    return condition
+
+# speedlist = iter(Df_B['speed'],Df_B['variance'])
+#
+# for speed,var in speedlist:
+#     loop.findcondition(speed,0.0001)
+
+# ConditionDict = {}
+# for row in Df_A.itertuples(name="Condition"):
+#     print(row)
+#     print(row.speed)
+#     if row.speed == "Low" and row.variance=="Low":
+#         print("you got it!")
+#         cond = 1
+#         ConditionDict.update({row.speed : cond})
+#     elif row.speed == "Med" and row.variance=="Low":
+#         print("It's the second one")
+#         cond = 0
+#         ConditionDict[row.Index]= cond
+#     else:
+#         cond = 3
+#         row = row._replace(condition = 3)
+#         print("Nothing yet")
+#     print(row, ConditionDict)
